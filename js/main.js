7d4f6aad940c55ec4cb2c3dece345ac52184b596
Milouyg <33333@ma-web.nl>
2023-05-12 13:30:10 +0200

    Added icons to header

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ class API{
 }
 
 class Agenda{
-    rendered;
+    renderer;
     header;
     month;
 
@@ -34,24 +34,52 @@ class Agenda{
 
     constructor(data) {
         this.htmlElement = document.createElement("article");
+        this.htmlElement.classList.add("agenda");
         this.data = data;
-        console.log(data)
-        this.rendered = new Renderer();
-        this.header = new Header(data.name);
+        this.renderer = new Renderer();
+        this.renderer.render("body", this.htmlElement);
+        this.header = new Header(this, data.name);
         this.month = new Month(this, data.days);
+    }
+
+    render(placeToRender, whatToRender) {
+        this.renderer.render(placeToRender, whatToRender);
     }
 }
 
 class Renderer{
-
+    render(placeToRender, whatToRender) {
+        document.querySelector(placeToRender).appendChild(whatToRender);
+    }
 }
 
 class Header{
     nameOfMonth;
     htmlElement;
+    agenda;
+    htmlElement__p;
+    htmlElement__iconLeft;
+    htmlElement__iconRight;
 
-    constructor(nameOfMonth){
+    constructor(agenda, nameOfMonth) {
+        this.agenda = agenda;
         this.htmlElement = document.createElement("header");
+        this.htmlElement.classList.add("agenda__header");
+        this.htmlElement__iconLeft = document.createElement("p");
+        this.htmlElement__iconLeft.classList = "agenda__icon"
+        this.htmlElement__p = document.createElement("p");
+        this.htmlElement__p.classList = "agenda__p"
+        this.htmlElement__iconRight = document.createElement("p");
+        this.htmlElement__iconRight.classList = "agenda__icon"
+        this.agenda.render(".agenda", this.htmlElement);
+
+        this.agenda.render(".agenda__header", this.htmlElement__iconLeft);
+        this.agenda.render(".agenda__header", this.htmlElement__p);
+        this.agenda.render(".agenda__header", this.htmlElement__iconRight);
+        this.htmlElement__iconLeft.innerText = "<";
+        this.htmlElement__p.innerText = "Agenda";
+        this.htmlElement__iconRight.innerText = ">";
+
         this.nameOfMonth = nameOfMonth;
     }
 
@@ -65,20 +93,32 @@ class Month{
 
     constructor(agenda, numberOfDays) {
         this.htmlElement = document.createElement("ul");
+        this.htmlElement.classList.add("agenda__month");
         this.numberOfDays = numberOfDays;
         this.agenda = agenda;
-        for (let i = 0; i <= this.numberOfDays; i++){
-            this.days.push(new Day(this));
+        this.agenda.render(".agenda", this.htmlElement);
+        for (let i = 1; i <= this.numberOfDays; i++){
+            this.days.push(new Day(this, i));
         }
+    }
+
+    renderDays(placeToRender,whatToRender) {
+        this.agenda.render(placeToRender, whatToRender);
     }
 }
 
 class Day{
     month;
     htmlElement;
-    constructor(month) {
+    dayNumber;
+
+    constructor(month, dayNumber) {
+        this.dayNumber = dayNumber;
         this.htmlElement = document.createElement("li");
+        this.htmlElement.classList.add("agenda__day");
+        this.htmlElement.innerText = this.dayNumber;
         this.month = month;
+        this.month.renderDays(".agenda__month", this.htmlElement);
     }
 
 }
